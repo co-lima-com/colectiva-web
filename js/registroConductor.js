0bf89257d0957efaa572c3482f647a308b1bb540
johel cueva <johel.cueva@gmail.com>
2024-10-17 04:13:57 -0500

    primeros agregados de css y cambios en todos los html

--- js/registroConductor.js
+++ js/registroConductor.js
@@ -2,7 +2,9 @@
 
 //Seleccionamos el formulario
 const form = document.getElementById('registrationForm');
+const buttonSubmit = document.getElementById('storeButton');
 
+buttonSubmit.disabled = true;
 
 // Agregamos un evento de submit al formulario
 form.addEventListener('submit', (e) => {
@@ -39,11 +41,28 @@ form.addEventListener('submit', (e) => {
         driverSoatCara,
         driverSoatContraCara
     };
-  
 
+    let allFieldsFilled = true;
+    [driverNombre, driverApellidoPaterno, driverApellidoMaterno, driverFechaNacimiento, driverDNI, driverDireccion, driverEmail, driverPassword, driverDniCara, driverDniContraCara, driverSoatCara, driverSoatContraCara].forEach((value) => {
+      if (value.trim() === '') {
+        allFieldsFilled = false;
+      }
+    });
+  
+    // Si todos los campos están llenos, habilitamos el botón
+    if (allFieldsFilled) {
+      buttonSubmit.disabled = false;
+      
+    } else {
+      // De lo contrario, deshabilitamos el botón
+      buttonSubmit.disabled = true;
+      
+    }
+
+    localStorage.setItem('formData', JSON.stringify(formData));
 
     // Almaceno el objeto en un localstorage
-    localStorage.setItem('formData', JSON.stringify(formData));
+    
 
     // //Recuperar del localstorage
     // const formDataString = localStorage.getItem('formData');
@@ -54,6 +73,6 @@ form.addEventListener('submit', (e) => {
     // Para imprimir y ver errores
     console.log(formData);
     alert('¡Registro exitoso! Te contactaremos a la brevedad.');
-    
+
 }
 );
